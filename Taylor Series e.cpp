2da9fc59c1edc8056eb6a1f60dee2fb3f1dc1247
kartik-soni1707 <its.kartiksoni@gmail.com>
2019-07-10 13:50:31 +0530

    Add files via upload

---
+++ Taylor Series e.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+double e(double x,double n){
+static double p=1,f=1;
+double r;
+if (n==0)
+    return 1;
+else{
+    r=e(x,n-1);
+    p=p*x;
+    f=f*n;
+    return r+(p/f);
+}
+
+
+}
+
+int main()
+{
+    cout<<e(4,15);
+}
