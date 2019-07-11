9f4f406e6b573818c9f37cf32941dd5988d45e3f
kartik-soni1707 <its.kartiksoni@gmail.com>
2019-07-11 09:57:44 +0530

    Add files via upload

---
+++ nCr.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+int c(int n, int r){
+if (r==0||n==r)
+    return 1;
+{
+    return c(n-1,r-1)+c(n-1,r);
+
+}
+}
+int main()
+{
+
+    cout<<c(4,1);
+
+}
