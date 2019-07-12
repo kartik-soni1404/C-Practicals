62837d1d498aae992528baf9433ea3c5a8e6fcb4
kartik-soni1707 <its.kartiksoni@gmail.com>
2019-07-12 12:19:25 +0530

    O(2^n)

---
+++ Tower Of Hanoi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<stdio.h>
+using namespace std;
+
+void TOH(int n,int a,int b,int c){
+if(n>0){
+    TOH(n-1,a,c,b);
+    cout<<a<<" to "<<c<<"\n";
+    TOH(n-1,b,a,c);
+
+}
+
+
+}
+int main()
+{
+
+    TOH(3,1,2,3);
+
+}
